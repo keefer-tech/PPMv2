86aa70dd2680b828fe54419346ab3e458920d941
Daniel John Keefer <xxkeefer.code@gmail.com>
2020-10-20 21:16:48 +1000

    added loading screen

---
+++ frontend/src/components/GuestComponents/LoadingAnimation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function LoadingAnimation() {
+  return (
+    <section class="hero is-info is-bold is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Hang tight,</h1>
+          <h2 class="subtitle">Numbers are being crunched.</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
--- frontend/src/scenes/Guest.js
+++ frontend/src/scenes/Guest.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Tutorial from "../components/GuestComponents/Tutorial";
 import CollectUsers from "../components/GuestComponents/CollectUsers";
+import LoadingAnimation from "../components/GuestComponents/LoadingAnimation";
 export default function Guest() {
+  const [loading, setLoading] = useState(false);
+
+  function onRequest() {
+    setLoading(true);
+  }
+
   return (
     <div className="tile is-ancestor">
       <div className="tile is-parent is-vertical notification is-dark is-3">
         <div className="tile is-child is-vertical">
           <h1 className="title">Users to compare:</h1>
-          <CollectUsers />
+          <CollectUsers triggerLoading={onRequest} />
         </div>
       </div>
       <div className="tile is-parent is-vertical notification is-success">
         <div className="tile is-child">
-          <Tutorial />
+          {!loading && <Tutorial />}
+          {loading && <LoadingAnimation />}
         </div>
       </div>
     </div>
